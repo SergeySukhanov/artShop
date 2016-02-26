eeff87eacf9f4087225136f520012cf23811089d
sergey.n.sukhanov <sergey.n.sukhanov@gmail.com>
2016-02-26 13:45:07 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+/**
+ * Short description text.
+ *
+ * Long detailed description text for the specific class file.
+ *
+ * @author SNSukhanov <sergey.n.sukhanov@firstlinesoftware.com>
+ * @version 02/26/2016
+ * @see
+ *
+ * © 2016 Caple International B.V. - All Rights Reserved
+ * See LICENSE file or http://caple.co for further details
+ */
